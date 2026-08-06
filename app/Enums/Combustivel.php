d7a46e6c1bf67a88366406d030fc8cff3cdbd4d2
LucasAAlv <lucasev76@gmail.com>
2026-08-05 22:32:50 -0300

    feat: add Cambio and Combustivel enums

---
+++ app/Enums/Combustivel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+enum Combustivel: string
+{
+    case Gasolina = 'gasolina';
+    case Alcool = 'alcool';
+    case Flex = 'flex';
+    case Diesel = 'diesel';
+    case Hibrido = 'hibrido';
+    case Eletrico = 'eletrico';
+}
